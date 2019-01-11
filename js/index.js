9139663fc7de644c19bb9cd53a179b12c40bac5f
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-01-11 14:32:31 -0500

    Node.js - use require() instead of fs.read.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
-const v1 = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '../contexts/security-v1.jsonld'),
-    {encoding: 'utf8'}));
-
-const v2 = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '../contexts/security-v2.jsonld'),
-    {encoding: 'utf8'}));
-
-module.exports = {v1, v2};
+module.exports = {
+  v1: require('../contexts/security-v1.jsonld.json'),
+  v2: require('../contexts/security-v2.jsonld.json')
+};
