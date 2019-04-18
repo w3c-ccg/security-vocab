6e867d72b839912159a20e18b341ebf3b2cedb96
David I. Lehn <dlehn@digitalbazaar.com>
2019-04-18 17:17:04 -0400

    Add browser version and new contexts interface.
    - Build and distribute static browser version with all contexts.
    - Export a `contexts` Map associating ids to contexts.
    - Remove exported `v1` and `v2` in favor of new `contexts` Map.

--- js/index.js
+++ js/index.js
@@ -3,12 +3,18 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.v1 = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '../contexts/security-v1.jsonld'),
-    {encoding: 'utf8'}));
+const contexts = exports.contexts = new Map();
 
-exports.v2 = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '../contexts/security-v2.jsonld'),
-    {encoding: 'utf8'}));
+function _read(_path) {
+  return JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, _path),
+      {encoding: 'utf8'}));
+}
+
+contexts.set(
+  'https://w3id.org/security/v1',
+  _read('../contexts/security-v1.jsonld'));
+contexts.set(
+  'https://w3id.org/security/v2',
+  _read('../contexts/security-v2.jsonld'));
