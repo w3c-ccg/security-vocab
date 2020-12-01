91f288fe497c7b3d6ae5c91ecfc7ba0bb5a8d1a6
Orie Steele <orie@transmute.industries>
2020-12-01 12:06:26 -0600

    chore: add ci tests for jsonld-sigantures and vc-js

---
+++ js/__fixtures__/documentLoader.js
@@ -0,0 +1,57 @@
+const {
+  documentLoaderFactory,
+  contexts,
+} = require("@transmute/jsonld-document-loader");
+const fs = require("fs");
+const path = require("path");
+const didKeyEd25519 = require("@transmute/did-key-ed25519");
+const didKeyWebCrypto = require("@transmute/did-key-web-crypto");
+
+const securityV3Unstable = JSON.parse(
+  fs
+    .readFileSync(
+      path.resolve(__dirname, "../../contexts/security-v3-unstable.jsonld")
+    )
+    .toString()
+);
+
+const credentialsV2 = JSON.parse(
+  fs
+    .readFileSync(
+      path.resolve(__dirname, "../../contexts/credentials-v2-unstable.jsonld")
+    )
+    .toString()
+);
+
+const documentLoader = documentLoaderFactory.pluginFactory
+  .build({
+    contexts: {
+      ...contexts.W3C_Decentralized_Identifiers,
+      ...contexts.W3ID_Security_Vocabulary,
+    },
+  })
+  .addContext({
+    "https://www.w3.org/2018/credentials/v1": credentialsV2,
+    "https://w3id.org/security/v3-unstable": securityV3Unstable,
+  })
+  .addResolver({
+    "did:key:z6": {
+      resolve: async (uri) => {
+        const { didDocument } = await didKeyEd25519.driver.resolve(uri, {
+          accept: "application/did+ld+json",
+        });
+        return didDocument;
+      },
+    },
+    "did:key:zA": {
+      resolve: async (uri) => {
+        const { didDocument } = await didKeyWebCrypto.driver.resolve(uri, {
+          accept: "application/did+json",
+        });
+        return didDocument;
+      },
+    },
+  })
+  .buildDocumentLoader();
+
+module.exports = { documentLoader };
