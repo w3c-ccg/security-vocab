ce8dd85a6f881fcc378e817c7451f39aa68d4539
Orie Steele <orie@transmute.industries>
2020-12-06 19:52:25 -0600

    chore: fix tests

--- js/__tests__/bbs/bbs.test.js
+++ js/__tests__/bbs/bbs.test.js
@@ -4,29 +4,117 @@ const {
   BbsBlsSignature2020,
   BbsBlsSignatureProof2020,
   deriveProof,
-} = require("@mattrglobal/jsonld-signatures-bbs");
+} = require('@mattrglobal/jsonld-signatures-bbs');
 
-const fs = require("fs");
-const path = require("path");
-const vcjs = require("@transmute/vc.js");
+const fs = require('fs');
+const path = require('path');
+const vcjs = require('@transmute/vc.js');
 
 const vcld = vcjs.ld;
 
-const didKeyFixture = require("../../__fixtures__/did-key-bls12381.json");
+const didKeyFixture = require('../../__fixtures__/did-key-bls12381.json');
 
-const k0 = didKeyFixture[0].g2["application/did+ld+json"];
+const k0 = didKeyFixture[0].g2['application/did+ld+json'];
 k0.id = k0.controller + k0.id;
 
-let credentialTemplate = require("../../__fixtures__/credentials/bbs/credentialTemplate.json");
-let expectedVerifiableCredential = require("../../__fixtures__/credentials/bbs/verifiableCredential.json");
+const credentialTemplate = require('../../__fixtures__/credentials/bbs/credentialTemplate.json');
 let verifiableCredential;
 
+const documentLoader = url => {
 
-describe.skip("BBS", ()=>{
+  if(url === 'https://www.w3.org/2018/credentials/v2') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/credentials-v2-unstable.json'),
+    };
+  }
 
-  it("can issue, verify", async () => {
+  if(url === 'https://w3id.org/security/v1') {
+    return {
+      documentUrl: url,
+      document: JSON.parse(
+        fs
+          .readFileSync(
+            path.resolve(
+              __dirname,
+              '../../../contexts/security-v1.jsonld'
+            )
+          )
+          .toString()
+      ),
+    };
+  }
+
+  if(url === 'https://w3id.org/security/v2') {
+    return {
+      documentUrl: url,
+      document: JSON.parse(
+        fs
+          .readFileSync(
+            path.resolve(
+              __dirname,
+              '../../../contexts/security-v2.jsonld'
+            )
+          )
+          .toString()
+      ),
+    };
+  }
+
+  // This won't be necessary soon
+  // see https://github.com/mattrglobal/jsonld-signatures-bbs/issues/93
+  if(url === 'https://w3id.org/security/v3-unstable') {
+    return {
+      documentUrl: url,
+      document: JSON.parse(
+        fs
+          .readFileSync(
+            path.resolve(
+              __dirname,
+              '../../../contexts/security-v3-unstable.jsonld'
+            )
+          )
+          .toString()
+      ),
+    };
+  }
+
+  if(url === 'https://www.w3.org/ns/did/v1') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/did-v1.json'),
+    };
+  }
+
+  if(url === 'https://w3id.org/citizenship/v1') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/citizenship-v1.json'),
+    };
+  }
+
+  if(
+    url.indexOf(
+      'did:key:z5TcESXuYUE9aZWYwSdrUEGK1HNQFHyTt4aVpaCTVZcDXQmUheFwfNZmRksaAbBneNm5KyE52SdJeRCN1g6PJmF31GsHWwFiqUDujvasK3wTiDr3vvkYwEJHt7H5RGEKYEp1ErtQtcEBgsgY2DA9JZkHj1J9HZ8MRDTguAhoFtR4aTBQhgnkP4SwVbxDYMEZoF2TMYn3s'
+    ) === 0
+  ) {
+    const didDoc =
+      didKeyFixture[0].resolution['application/did+ld+json'].didDocument;
+
+    return {
+      documentUrl: url,
+      document: didDoc,
+    };
+  }
+
+  throw new Error('Unsupported context ' + url);
+};
+
+describe('BBS', () => {
+
+  it('can issue, verify', async () => {
     const keyPair = await new Bls12381G2KeyPair(k0);
-    const suite = new BbsBlsSignature2020({ key: keyPair, date: credentialTemplate.issuanceDate });
+    const suite = new BbsBlsSignature2020({key: keyPair, date: credentialTemplate.issuanceDate});
     suite.date = credentialTemplate.issuanceDate;
     verifiableCredential = await vcld.issue({
       credential: {
@@ -40,138 +128,37 @@ describe.skip("BBS", ()=>{
         },
       },
       suite,
-      documentLoader: (url) => {
-        if (url === "https://www.w3.org/2018/credentials/v2") {
-          return {
-            documentUrl: url,
-            document: require("../../__fixtures__/contexts/credentials-v2-unstable.json"),
-          };
-        }
-        if (url === "https://w3id.org/security/v2") {
-          return {
-            documentUrl: url,
-            document: JSON.parse(
-              fs
-                .readFileSync(
-                  path.resolve(
-                    __dirname,
-                    "../../../contexts/security-v3-unstable.jsonld"
-                  )
-                )
-                .toString()
-            ),
-          };
-        }
-  
-        if (url === "https://w3id.org/security/v3-unstable") {
-          return {
-            documentUrl: url,
-            document: JSON.parse(
-              fs
-                .readFileSync(
-                  path.resolve(
-                    __dirname,
-                    "../../../contexts/security-v3-unstable.jsonld"
-                  )
-                )
-                .toString()
-            ),
-          };
-        }
-  
-        if (url === "https://w3id.org/citizenship/v1") {
-          return {
-            documentUrl: url,
-            document: require("../../__fixtures__/contexts/citizenship-v1.json"),
-          };
-        }
-  
-        throw new Error("Unsupported context " + url);
-      },
+      documentLoader
     });
-    console.log(JSON.stringify(verifiableCredential, null, 2));
-    
-    // const credentialVerified = await vcld.verifyCredential({
-    //   credential: { ...verifiableCredential },
-    //   suite: new BbsBlsSignature2020({}),
-    //   documentLoader: (url) => {
-    //     if (
-    //       url.indexOf(
-    //         "did:key:z5TcESXuYUE9aZWYwSdrUEGK1HNQFHyTt4aVpaCTVZcDXQmUheFwfNZmRksaAbBneNm5KyE52SdJeRCN1g6PJmF31GsHWwFiqUDujvasK3wTiDr3vvkYwEJHt7H5RGEKYEp1ErtQtcEBgsgY2DA9JZkHj1J9HZ8MRDTguAhoFtR4aTBQhgnkP4SwVbxDYMEZoF2TMYn3s"
-    //       ) === 0
-    //     ) {
-    //       const didDoc =
-    //         didKeyFixture[0].resolution["application/did+ld+json"].didDocument;
-    //       didDoc["@context"].push(
-    //         "https://w3c-ccg.github.io/ldp-bbs2020/context/v1"
-    //       );
-    //       return {
-    //         documentUrl: url,
-    //         document: didDoc,
-    //       };
-    //     } else {
-    //       return documentLoader(url);
-    //     }
-    //   },
-    // });
-    // expect(credentialVerified.verified).toBe(true);
+
+    const credentialVerified = await vcld.verifyCredential({
+      credential: {...verifiableCredential},
+      suite: new BbsBlsSignature2020({}),
+      documentLoader,
+    });
+
+    expect(credentialVerified.verified).toBe(true);
   });
-  
-  // it('can derive, present', async () => {
-  //   //Derive a proof
-  //   const deriveProofFrame = f;
-  //   const derivedProofCredential = await deriveProof(
-  //     verifiableCredential,
-  //     deriveProofFrame,
-  //     {
-  //       suite: new BbsBlsSignatureProof2020(),
-  //       documentLoader: (url) => {
-  //         if (
-  //           url.indexOf(
-  //             'did:key:z5TcESXuYUE9aZWYwSdrUEGK1HNQFHyTt4aVpaCTVZcDXQmUheFwfNZmRksaAbBneNm5KyE52SdJeRCN1g6PJmF31GsHWwFiqUDujvasK3wTiDr3vvkYwEJHt7H5RGEKYEp1ErtQtcEBgsgY2DA9JZkHj1J9HZ8MRDTguAhoFtR4aTBQhgnkP4SwVbxDYMEZoF2TMYn3s'
-  //           ) === 0
-  //         ) {
-  //           const didDoc =
-  //             didKeyFixture[0].resolution['application/did+ld+json'].didDocument;
-  //           didDoc['@context'].push(
-  //             'https://w3c-ccg.github.io/ldp-bbs2020/context/v1'
-  //           );
-  //           return {
-  //             documentUrl: url,
-  //             document: didDoc,
-  //           };
-  //         } else {
-  //           return documentLoader(url);
-  //         }
-  //       },
-  //     }
-  //   );
-  //   // console.log(JSON.stringify(derivedProofCredential, null, 2))
-  //   const credentialVerified = await vcld.verifyCredential({
-  //     credential: { ...derivedProofCredential },
-  //     suite: new BbsBlsSignatureProof2020({}),
-  //     documentLoader: (url) => {
-  //       if (
-  //         url.indexOf(
-  //           'did:key:z5TcESXuYUE9aZWYwSdrUEGK1HNQFHyTt4aVpaCTVZcDXQmUheFwfNZmRksaAbBneNm5KyE52SdJeRCN1g6PJmF31GsHWwFiqUDujvasK3wTiDr3vvkYwEJHt7H5RGEKYEp1ErtQtcEBgsgY2DA9JZkHj1J9HZ8MRDTguAhoFtR4aTBQhgnkP4SwVbxDYMEZoF2TMYn3s'
-  //         ) === 0
-  //       ) {
-  //         const didDoc =
-  //           didKeyFixture[0].resolution['application/did+ld+json'].didDocument;
-  //         didDoc['@context'].push(
-  //           'https://w3c-ccg.github.io/ldp-bbs2020/context/v1'
-  //         );
-  //         return {
-  //           documentUrl: url,
-  //           document: didDoc,
-  //         };
-  //       } else {
-  //         return documentLoader(url);
-  //       }
-  //     },
-  //   });
-  //   // console.log(JSON.stringify(credentialVerified, null, 2))
-  //   expect(credentialVerified.verified).toBe(true);
-  // });
-  
-})
+
+  it('can derive, present', async () => {
+    //Derive a proof
+    const deriveProofFrame = require('../../__fixtures__/credentials/bbs/deriveProofFrame.json');
+    const derivedProofCredential = await deriveProof(
+      verifiableCredential,
+      deriveProofFrame,
+      {
+        suite: new BbsBlsSignatureProof2020(),
+        documentLoader,
+      }
+    );
+    // console.log(JSON.stringify(derivedProofCredential, null, 2))
+    const credentialVerified = await vcld.verifyCredential({
+      credential: {...derivedProofCredential},
+      suite: new BbsBlsSignatureProof2020({}),
+      documentLoader,
+    });
+    // console.log(JSON.stringify(credentialVerified, null, 2))
+    expect(credentialVerified.verified).toBe(true);
+  });
+
+});
--- js/__tests__/p384/p384.test.js
+++ js/__tests__/p384/p384.test.js
@@ -1,74 +1,75 @@
+/* eslint-disable max-len */
 const {
-    JsonWebSignature,
-    JsonWebKey,
-  } = require('@transmute/json-web-signature-2020');
+  JsonWebSignature,
+  JsonWebKey,
+} = require('@transmute/json-web-signature-2020');
 
-  const fs = require("fs");
-  const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const vcjs = require('@transmute/vc.js');
 
-const didKeyFixture = require("../../__fixtures__/did-key-p384.json");
+const didKeyFixture = require('../../__fixtures__/did-key-p384.json');
 
 const vcld = vcjs.ld;
 
-const k0 = didKeyFixture[0].keypair["application/did+json"];
+const k0 = didKeyFixture[0].keypair['application/did+json'];
 k0.id = k0.controller + k0.id;
 
-let credentialTemplate = require("../../__fixtures__/credentials/p384/credentialTemplate.json");
+const credentialTemplate = require('../../__fixtures__/credentials/p384/credentialTemplate.json');
 let verifiableCredential;
 
-const documentLoader = (url) => {
-        if (url === "https://www.w3.org/2018/credentials/v2") {
-          return {
-            documentUrl: url,
-            document: require("../../__fixtures__/contexts/credentials-v2-unstable.json"),
-          };
-        }
+const documentLoader = url => {
+  if(url === 'https://www.w3.org/2018/credentials/v2') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/credentials-v2-unstable.json'),
+    };
+  }
 
-        if (url === "https://www.w3.org/ns/odrl.jsonld") {
-          return {
-            documentUrl: url,
-            document: require("../../__fixtures__/contexts/odrl.json"),
-          };
-        }
+  if(url === 'https://www.w3.org/ns/odrl.jsonld') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/odrl.json'),
+    };
+  }
 
-        if (url === "https://www.w3.org/2018/credentials/examples/v1") {
-          return {
-            documentUrl: url,
-            document: require("../../__fixtures__/contexts/example-v1.json"),
-          };
-        }
+  if(url === 'https://www.w3.org/2018/credentials/examples/v1') {
+    return {
+      documentUrl: url,
+      document: require('../../__fixtures__/contexts/example-v1.json'),
+    };
+  }
 
-        if (url === "https://w3id.org/security/v2") {
-        return {
-          documentUrl: url,
-          document: JSON.parse(
-            fs
-              .readFileSync(
-                path.resolve(
-                  __dirname,
-                  "../../../contexts/security-v3-unstable.jsonld"
-                )
-              )
-              .toString()
-          ),
-        };
-      }
-        console.error(url);
-        throw new Error("Unsupported context " + url);
-      }
-    
+  if(url === 'https://w3id.org/security/v2') {
+    return {
+      documentUrl: url,
+      document: JSON.parse(
+        fs
+          .readFileSync(
+            path.resolve(
+              __dirname,
+              '../../../contexts/security-v3-unstable.jsonld'
+            )
+          )
+          .toString()
+      ),
+    };
+  }
+  console.error(url);
+  throw new Error('Unsupported context ' + url);
+};
 
+// this test is broken, JsonWebSignature2020
+// suite needs to be updated.
+describe.skip('P384', () => {
 
-      describe.skip("P384", ()=>{
+  it('can issue and verify', async () => {
+    const key = await JsonWebKey.from(k0);
+    const suite = new JsonWebSignature({
+      key,
+    });
 
-it('can issue and verify', async () => {
-  const key = await JsonWebKey.from(k0);
-  const suite = new JsonWebSignature({
-    key,
-  });
- 
-  verifiableCredential = await vcld.issue({
+    verifiableCredential = await vcld.issue({
       credential: {
         ...credentialTemplate,
         issuer: {
@@ -81,9 +82,9 @@ it('can issue and verify', async () => {
       },
       suite,
       documentLoader
-    })
+    });
 
-    console.log(JSON.stringify(verifiableCredential, null, 2))
+    console.log(JSON.stringify(verifiableCredential, null, 2));
 
     const result = await vcld.verifyCredential({
       credential: verifiableCredential,
@@ -93,7 +94,6 @@ it('can issue and verify', async () => {
 
     // console.log(verifiableCredential, result)
 
-    console.log(JSON.stringify(result, null, 2))
-})
-})
-  
+    console.log(JSON.stringify(result, null, 2));
+  });
+});
