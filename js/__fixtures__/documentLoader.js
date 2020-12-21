8ba9a3e4a56d08d5175c06b1a6b6a2bc40a87ae5
Orie Steele <orie@transmute.industries>
2020-12-21 08:44:08 -0600

    chore: make document loader explicit

--- js/__fixtures__/documentLoader.js
+++ js/__fixtures__/documentLoader.js
@@ -1,64 +1,66 @@
-const {
-  documentLoaderFactory,
-  contexts,
-} = require('@transmute/jsonld-document-loader');
-const fs = require('fs');
-const path = require('path');
+const { documentLoaderFactory } = require("@transmute/jsonld-document-loader");
+const fs = require("fs");
+const path = require("path");
 
-const didKeyEd25519 = require('@transmute/did-key-ed25519');
-const didKeyWebCrypto = require('@transmute/did-key-web-crypto');
+const didKeyEd25519 = require("@transmute/did-key-ed25519");
+const didKeyWebCrypto = require("@transmute/did-key-web-crypto");
 
 const documentLoader = documentLoaderFactory.pluginFactory
-  .build({
-    contexts: {
-      ...contexts.W3C_Decentralized_Identifiers,
-      ...contexts.W3ID_Security_Vocabulary,
-    },
-  })
+  .build()
   .addContext({
-    // eslint-disable-next-line max-len
-    'https://www.w3.org/2018/credentials/v2': require('../__fixtures__/contexts/credentials-v2-unstable.json'),
-    'https://w3id.org/security/v3-unstable': JSON.parse(
+    "https://www.w3.org/ns/did/v1": require("../__fixtures__/contexts/did-v1.json"),
+    "https://www.w3.org/2018/credentials/v2": require("../__fixtures__/contexts/credentials-v2-unstable.json"),
+    "https://w3id.org/security/v1": JSON.parse(
       fs
         .readFileSync(
-          path.resolve(
-            __dirname,
-            '../../contexts/security-v3-unstable.jsonld'
-          )
+          path.resolve(__dirname, "../../contexts/security-v1.jsonld")
         )
         .toString()
     ),
-    // eslint-disable-next-line max-len
-    'https://w3id.org/citizenship/v1': require('../__fixtures__/contexts/citizenship-v1.json')
+    "https://w3id.org/security/v2": JSON.parse(
+      fs
+        .readFileSync(
+          path.resolve(__dirname, "../../contexts/security-v2.jsonld")
+        )
+        .toString()
+    ),
+    "https://w3id.org/security/v3-unstable": JSON.parse(
+      fs
+        .readFileSync(
+          path.resolve(__dirname, "../../contexts/security-v3-unstable.jsonld")
+        )
+        .toString()
+    ),
+    "https://w3id.org/citizenship/v1": require("../__fixtures__/contexts/citizenship-v1.json"),
   })
   .addResolver({
-    'did:example:abc': {
+    "did:example:abc": {
       resolve: async () => {
-        return require('./controllers/bls12381-g2.json');
+        return require("./controllers/bls12381-g2.json");
       },
     },
-    'did:example:def': {
+    "did:example:def": {
       resolve: async () => {
-        return require('./controllers/ed25519.json');
+        return require("./controllers/ed25519.json");
       },
     },
-    'did:example:ghi': {
+    "did:example:ghi": {
       resolve: async () => {
-        return require('./controllers/p384.json');
+        return require("./controllers/p384.json");
       },
     },
-    'did:key:z6': {
-      resolve: async uri => {
-        const {didDocument} = await didKeyEd25519.driver.resolve(uri, {
-          accept: 'application/did+ld+json',
+    "did:key:z6": {
+      resolve: async (uri) => {
+        const { didDocument } = await didKeyEd25519.driver.resolve(uri, {
+          accept: "application/did+ld+json",
         });
         return didDocument;
       },
     },
-    'did:key:zA': {
-      resolve: async uri => {
-        const {didDocument} = await didKeyWebCrypto.driver.resolve(uri, {
-          accept: 'application/did+json',
+    "did:key:zA": {
+      resolve: async (uri) => {
+        const { didDocument } = await didKeyWebCrypto.driver.resolve(uri, {
+          accept: "application/did+json",
         });
         return didDocument;
       },
@@ -66,4 +68,4 @@ const documentLoader = documentLoaderFactory.pluginFactory
   })
   .buildDocumentLoader();
 
-module.exports = {documentLoader};
+module.exports = { documentLoader };
